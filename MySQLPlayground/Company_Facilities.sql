0aeef71b28477736f2d78463242b0a3842f4d843
George <ggeorgiou.gr@gmail.com>
2023-07-12 12:41:38 +0300

    Company facilities 2

--- MySQLPlayground/Company_Facilities.sql
+++ MySQLPlayground/Company_Facilities.sql
@@ -4,7 +4,8 @@ USE company;
 SELECT * FROM facilities WHERE facid in (1,5);
 
 
-# 2. Write a query that will print the firstname, surname and joindate of the members table who joined after the start of September 2012
+# 2. Write a query that will print the firstname, surname and joindate of the members table 
+# who joined after the start of September 2012
 # My solution
 SELECT firstname, surname, joindate FROM members WHERE MONTH(joindate) >= 9;
 # Right solution
@@ -22,7 +23,8 @@ SELECT * FROM facilities WHERE name like ('Tennis');
 SELECT * FROM facilities WHERE name LIKE '%Tennis%';
 
 
-#5. Write a query that will print the name and the monthly maintenance of the facilities that are labelled as 'cheap' or 'expensive'. This is dependent on if their monthlymaintenance > 10;
+#5. Write a query that will print the name and the monthly maintenance of the facilities 
+# that are labelled as 'cheap' or 'expensive'. This is dependent on if their monthlymaintenance > 10;
 # My solution
 SELECT name AS 'Cheap', monthlymaintenance as 'Expensive' FROM facilities WHERE monthlymaintenance > 100;
 # Right solution
@@ -44,3 +46,22 @@ SELECT firstname, surname, joindate FROM members WHERE joindate =
 	SELECT MAX(joindate)
     FROM members
 );
+
+
+# 7. Write a query that will print the name and surname of All of the members
+# who have used a tennis court facilities without duplicates. Right solution is following.
+SELECT DISTINCT CONCAT(memes.firstname,' ',memes.surname) as member, facs.name AS facility
+FROM members memes 
+join bookings bks on memes.memid = bks.memid
+join facilities facs on bks.facid = facs.facid
+WHERE bks.facid in (0,1);
+
+
+# 8. Write a query that will print a Count of the recommendations each member has made.
+# My solution
+SELECT count(recommendedby) FROM members WHERE recommendedby IS NOT NULL;
+# Right solution
+SELECT recommendedby, count(*) 
+FROM members 
+WHERE recommendedby IS NOT NULL
+GROUP BY recommendedby;
